e1be419f3357599fbde1eaf2c80d70574dc85a5c
ZLO#DEV <59707245+g7AzaZLO@users.noreply.github.com>
2024-06-23 15:20:03 +0300

    Add files via upload

--- nubit_installer.sh
+++ nubit_installer.sh
@@ -1,27 +1,30 @@
-# Проверяем установлен ли Docker
-if ! command -v docker &> /dev/null
-then
-echo "Docker не установлен. Установка Docker..."
-sudo apt-get update
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER
-echo "Docker успешно установлен."
+if ! command -v docker &> /dev/null; then
+    echo "Docker не установлен. Установка Docker..."
+    sudo apt-get update
+    sudo apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    sudo usermod -aG docker $USER
+    echo "Docker успешно установлен."
 else
-echo "Docker уже установлен. Пропускаем установку."
+    echo "Docker уже установлен. Пропускаем установку."
 fi
+cat <<EOF | sudo docker build -t nubit -
+FROM alpine
+RUN apk add --no-cache curl
+CMD curl -sL1 https://nubit.sh | bash
+EOF
 echo "Создаем Docker контейнер и выполняем команду внутри него..."
-docker run --rm nubit curl -sL1 https://nubit.sh | bash
+sudo docker run --rm nubit
 echo "Команда выполнена в Docker контейнере."
 echo -e "\033[1;36m"
 echo -e "████╗░██████╗░███████╗████╗  ░█████╗░███████╗░█████╗░███████╗██╗░░░░░░█████╗░"
